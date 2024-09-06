21bb4785809b37920d70cdb0d16e0ff047f72f6b
Manish Suthar <manishsuthar078@gmail.com>
2024-09-07 04:53:54 +0530

    image added

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,6 +1,15 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    -- showing hidden files by default
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_hidden = false,
+        hide_gitignored = false,
+        visible = true,
+      },
+    },
     window = {
       position = "right",
       width = 35,
@@ -8,15 +17,6 @@ return {
         -- disabling s (split view) for better navigation
         ["s"] = "",
         ["v"] = "open_vsplit",
-      },
-      -- show hidden files
-      filesystem = {
-        filtered_items = {
-          hide_dotfiles = false,
-          hide_hidden = false,
-          hide_gitignored = false,
-          visible = true,
-        },
       },
     },
   },
