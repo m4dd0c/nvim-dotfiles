c6dc21a389357509a9778ef2e0feffa9f7ea0d33
Manish Suthar <manishsuthar078@gmail.com>
2024-11-25 22:46:59 +0530

    impr: jj -> normal mode in terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,62 +3,68 @@
 -- Add any additional keymaps here
 
 local opts = { silent = false, noremap = true }
+local t_opts = { silent = true }
 local keymap = vim.keymap
 
--- saving on jk
+-- Enter normal mode in terminal
+keymap.set('t', 'jj', [[<C-\><C-n>]], t_opts)
+
+-- Saving on jk
 keymap.set("i", "jk", "<cmd>w<cr><ESC>", opts)
--- enter normal mode
+
+-- Enter normal mode
 keymap.set("i", "jj", "<ESC>", opts)
 keymap.set("i", "JJ", "<ESC>", opts)
 
--- goto single char ahead in insertmode
+-- Go to single char ahead in insertmode
 keymap.set("i", "kk", "<C-o>a", opts)
 
--- goto end of line in insertmode
+-- Go to end of line in insertmode
 keymap.set("i", "KK", "<C-o>A", opts)
 
--- jumplist
+-- Jumplist
 keymap.set("n", "<C-m>", "<C-o>", opts)
 
 -- Restart LSP when backslash pressed twice
 keymap.set("n", "\\\\", "<cmd>LspRestart<cr>", opts)
 
--- select all
+-- Select all
 keymap.set("n", "<C-a>", "gg<S-v>G", opts)
 
--- split window
+-- Split window
 keymap.set("n", "ss", ":vsplit<Return>", opts)
 
--- goto window
+-- Go to window
 keymap.set("n", "sh", "<C-w>h", opts)
 keymap.set("n", "sj", "<C-w>j", opts)
 keymap.set("n", "sk", "<C-w>k", opts)
 keymap.set("n", "sl", "<C-w>l", opts)
 
--- flash jump mode s -> ;
+-- Flash jump mode s -> ;
 keymap.set("n", ";", "<cmd>lua require('flash').jump()<cr>", opts)
 keymap.set("x", ";", "<cmd>lua require('flash').jump()<cr>", opts)
 
--- tabs
+-- Tabs
 keymap.set("n", "te", ":tabedit<Return>", opts)
 keymap.set("n", "td", ":tabclose<Return>", opts)
 keymap.set("n", "<tab>", ":tabnext<Return>", opts)
 keymap.set("n", "<S-tab>", ":tabprev<Return>", opts)
 
--- delete current buffer
+-- Delete current buffer
 keymap.set('n', 'ff', function()
   require('snacks.bufdelete').delete()
 end, opts)
 
--- delete all buffer
+-- Delete all buffer
 keymap.set('n', 'fa', function()
   require('snacks.bufdelete').all()
 end, opts)
 
+-- Go to previously opened buffer
 keymap.set("n", "fd", "<cmd>b#<cr>", opts)
 
 -- Diagnostics
--- see line diagnostic, since K used for hover, J makes sense
+-- See diagnostic, since K used for hover, J makes sense
 keymap.set("n", "J", vim.diagnostic.open_float, opts)
 
 -- goto diagnostic
@@ -70,8 +76,9 @@ end, opts)
 keymap.set("n", "<leader>t", "<cmd>TodoTelescope<cr>", opts)
 
 -- for html-live-server
-keymap.set('n', "<leader>hs", ":LiveServerStart<CR>", opts)       -- start-live-server
-keymap.set('n', "<leader>he", ":LiveServerStop<CR>", opts)        -- stop/end-live-server
+keymap.set('n', "<leader>hs", ":LiveServerStart<CR>", opts) -- start-live-server
+keymap.set('n', "<leader>he", ":LiveServerStop<CR>", opts)  -- stop/end-live-server
+
 -- for markdown-live-server
 keymap.set('n', "<leader>ms", ":MarkdownPreview<CR>", opts)       -- start-live-server
 keymap.set('n', "<leader>me", ":MarkdownPreviewStop<CR>", opts)   -- stop/end-live-server
@@ -79,7 +86,6 @@ keymap.set('n', "<leader>mit", ":MDInsertToc<CR>", opts)          -- Insert Tabl
 keymap.set('n', "<leader>mil", ":MDListItemBelow<CR>", opts)      -- Insert a list item below
 keymap.set('n', "<leader>mct", ":MDTaskToggle<CR>", opts)         -- toggle checkbox
 keymap.set('n', "<leader>mcn", ":MDResetListNumbering<CR>", opts) -- Fix list numbering
-
 
 -- C++ code runner in LazyVim
 vim.api.nvim_set_keymap('n', '<leader>r', ':term g++ % -o %:r && %:r.exe<CR>', opts)
