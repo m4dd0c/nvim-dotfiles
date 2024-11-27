0191e84cc9e4a7e73ecc8a305795f7ab101e7dcc
Manish Suthar <manishsuthar078@gmail.com>
2024-11-27 20:44:56 +0530

    refector: better configuration used

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,7 +1,41 @@
-
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
   opts = {
+    popup_border_style = "rounded",
+    default_component_configs = {
+      icon = {
+        folder_closed = "󰉋",
+        folder_open = "󰷏", -- A stylish, open folder icon with a modern twist
+      },
+      indent = {
+        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+        expander_collapsed = "󱞩",
+        expander_expanded = "󱞩",
+        expander_highlight = "NeoTreeExpander",
+        with_markers = false,
+        indent_marker = "󱞩",
+        last_indent_marker = "󱞩",
+      },
+      git_status = {
+        symbols = {
+          added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+          modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+          -- Status type
+          unstaged = "",
+          staged = "󰄴",
+          untracked = "󱠇",
+          ignored = "",
+          conflict = "",
+          deleted = "",
+          renamed = "➜",
+        },
+      },
+    },
     -- showing hidden files by default
     filesystem = {
       filtered_items = {
@@ -9,6 +43,7 @@ return {
         hide_hidden = false,
         hide_gitignored = false,
         visible = true,
+        hide_by_filename = { ".DS_Store", "node_modules" },
       },
     },
     window = {
