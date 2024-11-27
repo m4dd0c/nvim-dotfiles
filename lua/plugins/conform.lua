b3fe09dc0901074a99acef4236e2abf84b2463aa
Manish Suthar <manishsuthar078@gmail.com>
2024-11-27 20:44:11 +0530

    refector: cmd and args removed, since username for window is changed (no-space in the pathname).

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,23 +1,25 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        lua = {
-          command = "C:\\Users\\Manish Suthar\\AppData\\Local\\nvim-data\\mason\\bin\\stylua.CMD",
-          args = { "--config-path", "C:\\Users\\Manish Suthar\\AppData\\Local\\nvim\\stylua.toml" }, -- adjust args as needed
-        },
-        javascript = {
-          command = '"C:\\Users\\Manish Suthar\\AppData\\Local\\nvim-data\\mason\\bin\\prettier.CMD"',
-          args = { "--stdin-filepath", "%filepath" }, -- specify file path argument
-          stdin = true,                               -- enables standard input for Prettier
-        },
-        typescript = {
-          command = '"C:\\Users\\Manish Suthar\\AppData\\Local\\nvim-data\\mason\\bin\\prettier.CMD"',
-          args = { "--stdin-filepath", "%filepath" },
-          stdin = true,
-        },
-      },
+  "stevearc/conform.nvim",
+  event = { "BufWritePre", "BufNewFile" },
+  -- This will provide type hinting with LuaLS
+  ---@module "conform"
+  ---@type conform.setupOpts
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      typescriptreact = { "prettier" },
+      javascriptreact = { "prettier" },
+      yaml = { "prettier" },
+      toml = { "prettier" },
+      markdown = { "prettier" },
+      json = { "prettier" },
+      shell = { "shfmt" },
+    },
+    -- Set default options
+    default_format_opts = {
+      lsp_format = "fallback",
     },
   },
 }
