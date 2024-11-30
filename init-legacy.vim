a964b4a97e6f56c3d78bd7f57553de820f8993f1
Manish Suthar <manishsuthar078@gmail.com>
2024-12-01 00:56:42 +0530

    feat: legacy vim added w/ some essential keymaps and options

---
+++ init-legacy.vim
@@ -0,0 +1,15 @@
+" keymaps
+" enter normal mode
+imap jj <Esc>
+imap JJ <Esc>
+" save file
+imap jk <Esc>:w<CR>
+imap JK <Esc>:w<CR>
+
+" hide highlight
+nnoremap <Esc> :noh<CR><Esc>
+
+" options
+set ignorecase
+set smartcase
+set noswapfile
--- pwsh.ps1
+++ pwsh.ps1
@@ -1,11 +1,13 @@
-# Profile.ps1 for PWSH
-
 # Aliases
-Set-Alias vim neovide
 Set-Alias less "C:\Program Files\git\usr\bin\less.exe"
 Set-Alias grep findstr
 Set-Alias touch New-Item
 Set-Alias rm Remove-Item
+
+# Native vim
+function vim {
+    nvim -u "$env:LOCALAPPDATA\nvim\init-legacy.vim" @args
+}
 
 # alias fn
 function pconf {
@@ -13,17 +15,20 @@ function pconf {
 }
 
 function nconf {
-    nvim "C:\Users\Manish Suthar\AppData\Local\nvim"
+    nvim "C:\Users\Manish Suthar\AppData\Local\nvim\config\keymaps.lua"
 }
 
+function gconf {
+    nvim "C:\Users\Manish Suthar\.glzr\glazewm\config.yaml"
+}
 
-# laod prompt config
+# Laod prompt config
 function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
 $PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'unicorn.omp.json'
 
 oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
 
-# importing modules
+# Importing modules
 Import-Module -Name terminal-icons
 
 # AutoCompletion using PSReadLine
@@ -32,15 +37,14 @@ Set-PSReadLineKeyHandler -Key RightArrow -Function forwardWord
 # Ctrl+z to go back single word (default).
 
 # Bind Alt+L to AcceptWholeSuggestion
-Set-PSReadLineKeyHandler -Chord Alt+l -Function AcceptSuggestion
+Set-PSReadLineKeyHandler -Chord ALT+";" -Function AcceptSuggestion
 
+# ClearScreen on ctrl+l
+Set-PSReadlineKeyHandler -Key 'Ctrl+l' -Function ClearScreen
 
-
-# change default open location
+# Change default open location
 Set-Location "D:"
 
-# allow-dubious-git to fix: detected dubious ownership in repository.
+# Allow-dubious-git to fix: detected dubious ownership in repository.
 $env:GIT_ALLOW_DUBIOUS=1
-
-
 
