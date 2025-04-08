9b71f7bbe1118b5f6895e07930af5fef92288ee7
Manish Kumar <77256586+m4dd0c@users.noreply.github.com>
2025-04-08 21:09:40 +0530

    feat: git aliases added

--- pwsh.ps1
+++ pwsh.ps1
@@ -4,25 +4,184 @@ Set-Alias grep findstr
 Set-Alias touch New-Item
 Set-Alias rm Remove-Item
 
-# Native vim
+# Git-Specific-Aliases
+
+function gst {
+	git status
+}
+function gaa {
+	git add .
+}
+function glo {
+	git log --oneline
+}
+function gph {
+	git push
+}
+function gpl {
+	git pull
+}
+function gba {
+	git branch -a
+}
+function gr {
+	git reset
+}
+
+
+# git commit -m & git push in single cmd
+function gcp {
+  param (
+    [string]$msg
+  )
+  git commit -m "$msg"
+  git push
+}
+
+# git add . and git commit -m in single cmd
+function gacp {
+  param (
+    [string]$msg
+  )
+  git add .
+  git commit -m "$msg"
+  git push
+}
+
+# Alias: git commit -m msg
+function gcmm {
+  param (
+    [string]$msg
+  )
+    git commit -m "$msg"
+}
+
+# Alias: git add file_path
+function ga {
+  param (
+    [string]$path
+  )
+    git add "$path"
+}
+
+# Alias: git diff file_path
+function gd {
+  param (
+    [string]$path
+  )
+    git diff "$path"
+}
+
+# Alias: git commit -am msg
+function gcma {
+  param (
+    [string]$msg
+  )
+    git commit -am "$msg"
+}
+
+
+# Alias: git checkout branch_name
+function gco {
+  param (
+    [string]$branch_name
+  )
+    git checkout "$branch_name"
+}
+
+# Alias: git checkout -b branch_name
+function gcob {
+  param (
+    [string]$branch_name
+  )
+    git checkout -b "$branch_name"
+}
+
+# Alias: git branch branch_name
+function gb {
+  param (
+    [string]$branch_name
+  )
+    git branch "$branch_name"
+}
+
+# NPM-Specific-Aliases
+function ns {
+	npm start
+}
+function nd {
+	npm run dev
+}
+function nb {
+	npm run build 
+}
+
+# Use legacy vim version
 function vim {
     nvim -u "$env:LOCALAPPDATA\nvim\init-legacy.vim" @args
 }
 
-# alias fn
+# Configurations-Specific-Aliases
+
+# Powershell Configuration
 function pconf {
-    nvim "C:\Users\Manish Suthar\.config\powershell\user_profile.ps1"
+    param (
+        [ValidateSet("cd", "vim", "nvim", "cat", "less")]
+        [string]$cmd
+    )
+
+    $filePath = "C:\Users\m4dd0c\.config\powershell\user_profile.ps1"
+
+    switch ($cmd) {
+        "cd" { Set-Location (Split-Path $filePath) }
+        "vim" { vim $filePath }
+        "nvim" { nvim $filePath }
+        "cat" { cat $filePath }
+        "less" { less $filePath }
+        default { vim $filePath }
+    }
 }
 
+# Neovim Configuration
 function nconf {
-    nvim "C:\Users\Manish Suthar\AppData\Local\nvim\config\keymaps.lua"
+    param (
+        [ValidateSet("cd", "vim", "nvim", "cat", "less")]
+        [string]$cmd
+    )
+
+    $filePath = "C:\Users\m4dd0c\AppData\Local\nvim\lua\config\keymaps.lua"
+
+    switch ($cmd) {
+        "cd" { Set-Location (Split-Path $filePath) }
+        "vim" { vim $filePath }
+        "nvim" { nvim $filePath }
+        "cat" { cat $filePath }
+        "less" { less $filePath }
+        default { vim $filePath }
+    }
 }
 
+
+# GlazeWM Configuration
 function gconf {
-    nvim "C:\Users\Manish Suthar\.glzr\glazewm\config.yaml"
+    param (
+     [ValidateSet("cd", "vim", "nvim", "cat", "less")]
+     [string]$cmd = "nvim"
+    )
+
+    $filePath = "C:\Users\m4dd0c\.glzr\glazewm\config.yaml"
+
+    switch ($cmd) {
+        "cd" { Set-Location (Split-Path $filePath) }
+        "vim" { vim $filePath }
+        "nvim" { nvim $filePath }
+        "cat" { cat $filePath }
+        "less" { less $filePath }
+        default { vim $filePath }
+    }
 }
 
-# Laod prompt config
+# Laod prompt config (oh-my-posh)
 function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
 $PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'unicorn.omp.json'
 
@@ -47,4 +206,3 @@ Set-Location "D:"
 
 # Allow-dubious-git to fix: detected dubious ownership in repository.
 $env:GIT_ALLOW_DUBIOUS=1
-
