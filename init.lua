af7140881b227d1c6d7416d75dee627362198086
Manish Suthar <manishsuthar078@gmail.com>
2025-05-14 01:40:16 +0530

    feat: required utils.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
 -- loading highlight groups
 require("config.colorscheme")
+
+-- loading utils fns
+require("config.utils")
